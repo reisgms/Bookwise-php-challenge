a243a2a4ab539308b6da9517b2513cee93ac0517
reisgms <reis.tfate@gmail.com>
2025-05-30 08:50:58 -0400

    corrigido erro da view de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,42 +1,41 @@
-@extends('layouts.app')
-
+@extends('layout.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Registrar</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Registrar</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Nome</label>
-                            <input type="text" class="form-control" name="name" required>
-                        </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nome</label>
+                                <input type="text" class="form-control" name="name" required>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="email" class="form-label">E-mail</label>
-                            <input type="email" class="form-control" name="email" required>
-                        </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">E-mail</label>
+                                <input type="email" class="form-control" name="email" required>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Senha</label>
-                            <input type="password" class="form-control" name="password" required>
-                        </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Senha</label>
+                                <input type="password" class="form-control" name="password" required>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="password_confirmation" class="form-label">Confirmar Senha</label>
-                            <input type="password" class="form-control" name="password_confirmation" required>
-                        </div>
+                            <div class="mb-3">
+                                <label for="password_confirmation" class="form-label">Confirmar Senha</label>
+                                <input type="password" class="form-control" name="password_confirmation" required>
+                            </div>
 
-                        <button type="submit" class="btn btn-primary">Registrar</button>
-                    </form>
+                            <button type="submit" class="btn btn-primary">Registrar</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
